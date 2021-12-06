fb979d11b9fe8961c0493923ffc4383a0166396c
rexzz180 <94366908+rexzz180@users.noreply.github.com>
2021-12-06 21:01:31 +0800

    Update car.py

--- car.py
+++ car.py
@@ -9,17 +9,17 @@ confirm_input=st.button('請輸入確認')
 if confirm_input:
  if gas <= car_simulation.get('gas_warning'):
   st.write('油箱只剩', gas, '格! 準備加油!!')
-  else:
-    st.write('油箱還剩', gas, '格。')
-  if speed>=car_simulation.get('speed_limit'):
-      st.write('即將超速')
-  else:
-      st.write('安全')
-  if temp>=car_simulation.get('temp_warning'):
-      st.write('過熱')
-  else:
-      st.write('正常')
-  if 轉速>=car_simulation.get('轉速'):
-      st.write('即將超過轉速')
-  else:
-      st.write('正常')
+ else:
+  st.write('油箱還剩', gas, '格。')
+ if speed>=car_simulation.get('speed_limit'):
+   st.write('即將超速')
+ else:
+   st.write('安全')
+ if temp>=car_simulation.get('temp_warning'):
+   st.write('過熱')
+ else:
+   st.write('正常')
+ if 轉速>=car_simulation.get('轉速'):
+   st.write('即將超過轉速')
+ else:
+   st.write('正常')
