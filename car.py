c16875b73c5358aba6766e1be983b5d83719b53f
rexzz180 <94366908+rexzz180@users.noreply.github.com>
2021-12-06 20:55:08 +0800

    Update car.py

--- car.py
+++ car.py
@@ -1,10 +1,10 @@
 import streamilt as st
 
 car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, '轉速':12000}
-gas = st.number_input('油量的資料收集:油箱滿是10格 =>'))
-speed= st.number_input('車速的資料收集:限速100 =>'))
-temp = st.number_input('溫度的資料收集:限溫30 =>'))
-轉速=st.number_input('轉速資料的收集:限轉12000 =>'))
+gas = st.number_input('油量的資料收集:油箱滿是10格 =>')
+speed= st.number_input('車速的資料收集:限速100 =>')
+temp = st.number_input('溫度的資料收集:限溫30 =>')
+轉速=st.number_input('轉速資料的收集:限轉12000 =>')
 
 if gas <= car_simulation.get('gas_warning'):
   st.write('油箱只剩', gas, '格! 準備加油!!')
